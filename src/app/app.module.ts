05ad54c9167abe3cdeb61a7a26b13abc5998b1cd
Gevorg Avetisyan <gevorg.avetisyan@synisys.com>
2019-03-15 12:37:26 +0400

    Structure add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { ProductComponent } from './product/product.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import {AboutUsComponent} from './pages/about-us/about-us.component';
+import {ContactInfoComponent} from './pages/contact-info/contact-info.component';
+import {DeliveryChartComponent} from './pages/delivery-chart/delivery-chart.component';
+import {FooterComponent} from './layout/footer/footer.component';
+import {HeaderComponent} from './layout/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    CategoriesComponent,
+    ProductComponent,
+    CalculatorComponent,
+    ProductListComponent,
+    AboutUsComponent,
+    ContactInfoComponent,
+    DeliveryChartComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/pages/delivery-chart/delivery-chart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-delivery-cart',
+  templateUrl: './delivery-chart.component.html',
+  styleUrls: ['./delivery-chart.component.css']
+})
+export class DeliveryChartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
