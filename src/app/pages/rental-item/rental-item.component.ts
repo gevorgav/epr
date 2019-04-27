997c39e3174a2b44ac2937b278d3c0043bc91c25
Gevorg <gevqwertyuiop@mail.ru>
2019-04-28 00:24:28 +0400

    bugfix23

--- src/app/pages/rental-item/rental-item.component.ts
+++ src/app/pages/rental-item/rental-item.component.ts
@@ -11,6 +11,7 @@ import {ZipCode} from '../../shared/model/delivery-chart.model';
 import {zip} from 'rxjs';
 import {CategoryService} from '../../shared/services/category.service';
 import {CategoryModel} from '../../shared/model/category.model';
+import {NgxGalleryAnimation} from 'ngx-gallery';
 
 declare var SEMICOLON: any;
 declare var $: any;
@@ -21,9 +22,24 @@ declare var $: any;
   styleUrls: ['./rental-item.component.css']
 })
 export class RentalItemComponent implements OnInit, AfterViewInit {
-
+  
   public galleryOptions = [
     {'imageSize': 'contain'},
+    {
+      width: '450px',
+      height: '400px',
+      thumbnailsColumns: 4,
+      imageAnimation: NgxGalleryAnimation.Slide
+    },
+    {
+      breakpoint: 800,
+      width: '100%',
+      height: '600px',
+      imagePercent: 80,
+      thumbnailsPercent: 20,
+      thumbnailsMargin: 20,
+      thumbnailMargin: 20
+    },
     {'breakpoint': 500, 'width': '300px', 'height': '300px', 'thumbnailsColumns': 3},
     {'breakpoint': 300, 'width': '100%', 'height': '200px', 'thumbnailsColumns': 2},
   ];
@@ -32,7 +48,7 @@ export class RentalItemComponent implements OnInit, AfterViewInit {
   public selectedProduct: ProductModel;
   public products: ProductModel[] = [];
   private title$ = this.route.paramMap;
-
+  
   constructor(private titleService: Title,
               private locationService: LocationDateService,
               private route: ActivatedRoute,
@@ -41,14 +57,14 @@ export class RentalItemComponent implements OnInit, AfterViewInit {
               private productService: ProductService,
               private categoryService: CategoryService) {
   }
-
+  
   ngOnInit() {
     this.getRouteParams();
     this.routingService.itemIdSubject.subscribe(res => {
       // console.log(res);
     });
   }
-
+  
   private getSelectedProduct(productPatch: string) {
     this.productService.getProductByPatch(productPatch).subscribe((res: ProductModel) => {
       if (!res) {
@@ -56,16 +72,16 @@ export class RentalItemComponent implements OnInit, AfterViewInit {
       }
       this.selectedProduct = res;
       this.titleService.setTitle(res.title);
-      this.categoryService.getCategoryByProductId(this.selectedProduct.id).subscribe((res: CategoryModel)=>{
-        this.categoryService.getCategoryItems(res.id).subscribe((res: ProductModel[])=>{
-          this.products = res.filter(product=> product.id !== this.selectedProduct.id);
+      this.categoryService.getCategoryByProductId(this.selectedProduct.id).subscribe((res: CategoryModel) => {
+        this.categoryService.getCategoryItems(res.id).subscribe((res: ProductModel[]) => {
+          this.products = res.filter(product => product.id !== this.selectedProduct.id);
           console.log(res);
-        })
+        });
       });
       this.initGallery();
     });
   }
-
+  
   ngAfterViewInit(): void {
     this.router.events.subscribe(res => {
       if (res instanceof NavigationEnd) {
@@ -75,17 +91,17 @@ export class RentalItemComponent implements OnInit, AfterViewInit {
       }
     });
   }
-
+  
   private getRouteParams() {
     this.title$.subscribe((params: ParamMap) => {
       this.getSelectedProduct(params.get('title'));
     });
   }
-
+  
   public navigate(id: number, title: string) {
     this.router.navigate(['/rental', title], {queryParams: {id: id}});
   }
-
+  
   private initGallery() {
     for (let image of this.selectedProduct.images) {
       this.galleryImages.push({
@@ -95,7 +111,7 @@ export class RentalItemComponent implements OnInit, AfterViewInit {
       });
     }
   }
-
+  
   isSpecified() {
     return this.locationService.isSpecified;
   }
