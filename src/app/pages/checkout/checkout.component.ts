9af5e93e1623cc54a8552f4eb08bce4fa9a5efc1
Gevorg <gevqwertyuiop@mail.ru>
2019-09-03 20:18:55 +0400

    for checkout

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare const Accept: any;
 
 @Component({
   selector: 'app-checkout',
@@ -10,6 +11,34 @@ export class CheckoutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.sendPaymentDataToAnet();
+    console.log('ssssss')
+  }
+  
+  sendPaymentDataToAnet(){
+    let authData = {};
+    authData['clientKey'] = "6863WfT8w92CaANw";
+    authData['apiLoginID'] = "6w38LfZ8v";
+    let cardData = {};
+    cardData['cardNumber'] = "5424000000000015";
+    cardData['month'] = "12";
+    cardData['year'] = "2020";
+    cardData['cardCode'] = "999";
+  
+  
+    let secureData = {};
+    secureData['authData'] = authData;
+    secureData['cardData'] = cardData;
+  
+    Accept.dispatchData(secureData,f);
+  }
+  
+  responseHandler(){
+    console.log('done');
   }
 
+}
+
+function f(e) {
+  console.log(e);
 }
