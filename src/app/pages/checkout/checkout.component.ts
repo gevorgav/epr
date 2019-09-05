9d75c75360fd925b9dadb392b6e90f687144d7ee
Gevorg <gevqwertyuiop@mail.ru>
2019-09-05 21:40:07 +0400

    pay

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -16,25 +16,25 @@ export class CheckoutComponent implements OnInit {
   }
   
   sendPaymentDataToAnet(){
-    let authData = {};
-    authData['clientKey'] = "9dQyqK5G2xP62erW9teH8hyjX5wq8jYjC7P7Z5d4dw9hyP76M4J2kWGRuQ5MFfSb";
-    authData['apiLoginID'] = "6w38LfZ8v";
-    let cardData = {};
-    cardData['cardNumber'] = "5424000000000015";
-    cardData['month'] = "12";
-    cardData['year'] = "2020";
-    cardData['cardCode'] = "999";
-  
-  
-    let secureData = {};
-    secureData['authData'] = authData;
-    secureData['cardData'] = cardData;
-  
-    Accept.dispatchData(secureData,f);
+    // let authData = {};
+    // authData['clientKey'] = "9dQyqK5G2xP62erW9teH8hyjX5wq8jYjC7P7Z5d4dw9hyP76M4J2kWGRuQ5MFfSb";
+    // authData['apiLoginID'] = "6w38LfZ8v";
+    // let cardData = {};
+    // cardData['cardNumber'] = "5424000000000015";
+    // cardData['month'] = "12";
+    // cardData['year'] = "2020";
+    // cardData['cardCode'] = "999";
+    //
+    //
+    // let secureData = {};
+    // secureData['authData'] = authData;
+    // secureData['cardData'] = cardData;
+    //
+    // Accept.dispatchData(secureData,f);
   }
   
   responseHandler(){
-    console.log('done');
+    // console.log('done');
   }
 
 }
